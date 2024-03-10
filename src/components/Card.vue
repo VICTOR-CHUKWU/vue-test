c8be9dd56f8ab23b3585d4fa850f68cb89003f86
VICTOR-CHUKWU <okenwa1993@gmail.com>
2024-03-10 21:15:56 +0100

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        <p>{{ gameNumber }}</p>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Card',
+    props: {
+        gameNumber: {
+      type: Number,
+      required: true,
+    }
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .card {
+    background: gray;
+    border-radius: 20px;
+    padding: 1rem;
+  }
+  </style>
+  
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,50 +1,78 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3 v-if="isUserWon">Congratulations you won the game</h3>
+   <div  class=" card-row">
+    <div v-for="(game, index) in gameNumbers" :key="index">
+      <Card  :gameNumber="game"/>
+    </div>
+   </div>
+    <div>
+      <p>Input your game value here</p>
+      <input type="text" v-model="userInput">
+      <button @click="getNumber">Submit</button>
+    </div>
+   
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Card from './Card.vue'
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  components: {Card},
+  data() {
+    return {
+      gameNumbers:[0,0,0],
+      userInput: '',
+      isUserWon: false
+    }
   },
+  methods: {
+    getNumber() {
+      this.gameNumbers = [0,0,0]
+      this.isUserWon = false
+      for(let i = 0; i< 3; i++){
+        const numberset = Math.round(Math.random() * 10)
+        this.gameNumbers[i] = numberset
+
+      }
+      let total = 0
+
+      for(let i = 0; i< this.gameNumbers.length; i++){
+        total += this.gameNumbers[i]
+        if(this.gameNumbers[i] === Number(this.userInput)){
+          this.isUserWon = true
+          break;
+        }
+        if(total === Number(this.userInput)){
+          this.isUserWon = true
+          break;
+        }
+        for(let j = i + 1; j < this.gameNumbers.length; j++){
+          if(this.gameNumbers[i] + this.gameNumbers[j] === Number(this.userInput)){
+            this.isUserWon = true
+          break;
+          }
+        }
+
+      }
+      
+    }
+  }
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.card-row {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
 h3 {
   margin: 40px 0 0;
+  color: green;
 }
 ul {
   list-style-type: none;
